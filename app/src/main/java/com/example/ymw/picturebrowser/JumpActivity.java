02fa363969741f7b5ef520a4241675afb4575a54
yinmingwang <972576849@qq.com>
2016-06-29 09:02:02 +0800

    picturebrowser

---
+++ app/src/main/java/com/example/ymw/picturebrowser/JumpActivity.java
@@ -0,0 +1,81 @@
+package com.example.ymw.picturebrowser;
+
+/**
+ * Created by ymw on 2016/6/28.
+ */
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+/**
+ * Created by ymw on 2016/6/28.
+ */
+public class JumpActivity extends Activity {
+    private int pos;
+    private int[] imgs = new int[] {
+            R.drawable.img01,
+            R.drawable.img02,
+            R.drawable.img03,
+            R.drawable.img04,
+            R.drawable.img05,
+            R.drawable.img06,
+            R.drawable.img07,
+            R.drawable.img08,
+            R.drawable.img09,
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.jump);
+        final ImageView  imgview = (ImageView) findViewById(R.id.imageView1);
+        Intent intent = getIntent();
+        pos =  (int) intent.getIntExtra("count", 0);
+        final ImageView imgview2 = (ImageView) findViewById(R.id.imageView2);
+        imgview2.setImageResource(R.drawable.img_return);
+        final ImageButton imagebutton1 = (ImageButton) findViewById(R.id.imageButton1);
+        imagebutton1.setImageResource(R.drawable.arrow_left);
+        final ImageButton imagebutton2 = (ImageButton) findViewById(R.id.imageButton2);
+        imagebutton2.setImageResource(R.drawable.arrow_right);
+        imagebutton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pos = pos - 1;
+                if (pos <= 0) {
+                    pos = 0;
+                    AlertDialog.Builder builder = new AlertDialog.Builder(JumpActivity.this);
+                    builder.setMessage("This has been the first");
+                    builder.show();
+                } else {
+                    imgview.setImageResource(imgs[pos]);
+                }
+            }
+        });
+        imagebutton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pos = pos + 1;
+                if (pos >= imgs.length) {
+                    pos = imgs.length - 1;
+                    AlertDialog.Builder builder = new AlertDialog.Builder(JumpActivity.this);
+                    builder.setMessage("This has been the last");
+                    builder.show();
+                } else {
+                    imgview.setImageResource(imgs[pos]);
+                }
+            }
+        });
+        imgview.setImageResource(imgs[pos]);
+        final Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/ymw/picturebrowser/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.ymw.picturebrowser;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class MainActivity extends Activity {
+    private int[] imgs = new int[] {
+            R.drawable.img01,
+            R.drawable.img02,
+            R.drawable.img03,
+            R.drawable.img04,
+            R.drawable.img05,
+            R.drawable.img06,
+            R.drawable.img07,
+            R.drawable.img08,
+            R.drawable.img09,
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        List<Map<String,Object>> listItems = new ArrayList<Map<String, Object>>();
+        for (int i = 0; i < imgs.length;i++) {
+            Map<String,Object> listItem = new HashMap<String, Object>();
+            listItem.put("image",imgs[i]);
+            listItems.add(listItem);
+        }
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this,listItems,R.layout.cell,new String[]{"image"},new int[]{R.id.imageView3});
+        GridView gridview = (GridView)findViewById(R.id.gridView1);
+        gridview.setAdapter(simpleAdapter);
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Bundle bundle=new Bundle();
+                bundle.putInt("count", position);
+                Intent intent = new Intent(MainActivity.this,JumpActivity.class);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
